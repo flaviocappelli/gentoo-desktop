2459374c140440f1b69e493d975d608d8e545117
Flavio Cappelli <flavio.cappelli@gmail.com>
2026-05-15 02:07:51 +0200

    Improve script

--- usr/local/bin/zfs_arc_usage.sh
+++ usr/local/bin/zfs_arc_usage.sh
@@ -2,7 +2,19 @@
 #
 # by F.C.
 
-arc_summary -s arc | grep -A 6 "ARC status:"
-
-echo -e "\nARC statistics:"
-arcstat
+if command -v zarcsummary &> /dev/null; then
+    # OpenZFS >= 2.4, see
+    # https://github.com/openzfs/zfs/releases/tag/zfs-2.4.0
+    zarcsummary -s arc | grep -A 6 "ARC status:"
+    echo -e "\nARC statistics:"
+    zarcstat
+elif command -v arc_summary &> /dev/null; then
+    # OpenZFS < 2.4
+    arc_summary -s arc | grep -A 6 "ARC status:"
+    echo -e "\nARC statistics:"
+    arcstat
+else
+    echo "Sorry, ZFS tools not installed"
+    exit 1
+fi
+exit 0
